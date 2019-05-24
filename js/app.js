42033199a655730346b369874728ab5d6f3a590c
Nikola <46688891+NikolaPantelic@users.noreply.github.com>
2019-05-24 14:24:07 +0200

    change

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let back = document.querySelectorAll(".big_box >button").style.backgroundColor="#F7F7F7";
+let circle = document.querySelectorAll(".small_circle").style.backgroundColor="#F7F7F7"
+let price = document.querySelectorAll(".price").style.color="#27c7ab";
+let buttonText =  document.querySelectorAll(".big_box > button").style.color="#27c7ab";
+let smallCircle = document.querySelectorAll(".small_circle").style.backgroundColor="#27c7ab";
+let border = document.querySelectorAll(".big_box").style.border="2px solid #27c7ab";
+let text = document.querySelectorAll(".big_box > h3").style.color="#F7F7F7";
+let bigCircle = document.querySelectorAll(".big_circle").style.border="2px solid #27c7ab";
+/*for (let i = 0; i < back.length; i++) {
+    back[i].addEventListener('mouseover', function () {
+        back.style("backgroundColor", "#24B99E");
+    var
+}
+*/
